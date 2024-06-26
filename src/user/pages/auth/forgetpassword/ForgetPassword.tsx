7b9f0d3df16bde69be8ba2cb5125099003864093
richcodes <ritchychiz43@gmail.com>
2024-06-26 03:40:02 +0100

    updated routes

--- src/user/pages/auth/forgetpassword/ForgetPassword.tsx
+++ src/user/pages/auth/forgetpassword/ForgetPassword.tsx
@@ -1,41 +1,54 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import * as Icon from "react-bootstrap-icons";
-import './ForgetPassword.css'
-import Button from '../../../components/button/Button';
-
+import "./ForgetPassword.css";
+import Button from "../../../components/button/Button";
+import Logo from "../../../../assets/images/logo2.png";
 const ForgetPassword = () => {
   return (
     <>
-        <div className="forgetPasswordPage">
-            <div className="forgetPasswordDetails">
-                <Link to="/" className='logo'>
-                    <Icon.Cart />
-                    Ritchy Stores
-                </Link>
-                <h4>Forget your Password?</h4>
-                <p>Enter your email below and we will send you a reset link</p>
-                <form className='forgetPassForm'>
-                    <input type="email" placeholder='Email'/>
-                    <Button label='Send' Icon={()=><Icon.SendFill/>}
-                        style={{padding:"10px",
-                        display:"flex",
-                        justifyContent:"center",
-                        gap:"4px"
-                        }}
-                        backgroundColor='#3874ff' 
-                        color='white'
-                        border='none'
-                        borderRadius="5px"
-                        cursor="pointer"
-                        fontSize="14px"
-
-
-                    />
-                </form>
+      <div className="forgetPasswordPage">
+        <Link to="/" className="logo">
+          <img src={Logo} alt="" />
+        </Link>
+        <div className="forgetPasswordDetails">
+            <div className="header flex flex-col items-center">
+                <h4 className="font-bold">Forget your Password?</h4>
+                <p>You will receive an email on how to reset your password</p>
             </div>
+          <form className="forgetPassForm">
+            <input type="email" placeholder="Enter your Email address" />
+            <Button
+              label="Send"
+              Icon={() => <Icon.SendFill />}
+              className="flex items-center justify-center p-2.5 gap-2"
+              backgroundColor="#3874ff"
+              color="white"
+              border="none"
+              borderRadius="5px"
+              cursor="pointer"
+              fontSize="14px"
+            />
+          </form>
+          <p
+            style={{
+              fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+              marginBottom:"4px",
+              float:"left"
+            }}
+            className="mt-3.5 text-sm flex justify-start"
+          >
+            Continue to
+            <Link
+              to="/auth/login"
+              style={{ color: "rgb(56, 116, 255)", marginLeft: "5px" }}
+            >
+              Login
+            </Link>
+          </p>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ForgetPassword
+export default ForgetPassword;
